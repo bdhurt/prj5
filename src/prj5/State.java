e2b3aa3b860b54aab62f1953e078fb6670d21174
bdhurt <82672726+bdhurt@users.noreply.github.com>
2021-04-16 15:21:51 -0400

    Began State
    Created State constructor, filled in basic getRace method (not final)

--- src/prj5/State.java
+++ src/prj5/State.java
@@ -1,5 +1,90 @@
 package prj5;
 
+/**
+ * State class, contains data for five races and the state
+ * 
+ * @author benjaminhurt
+ *
+ */
 public class State {
+    private LinkedList<Race> races;
+    private String name;
+
+    /**
+     * Constructor for the State class
+     * 
+     * @param stateName
+     *            name of the state
+     * @param white
+     *            white race object
+     * @param black
+     *            black race object
+     * @param latinx
+     *            latinx race object
+     * @param asian
+     *            asian race object
+     * @param other
+     *            other race object
+     */
+    public State(
+        String stateName,
+        Race white,
+        Race black,
+        Race latinx,
+        Race asian,
+        Race other) {
+        name = stateName;
+        races.add(white);
+        races.add(black);
+        races.add(latinx);
+        races.add(asian);
+        races.add(other);
+    }
+
+
+    public Race getRace(RaceEnum r) {
+        Race[] tempList;
+        State tempState;
+        tempState = this;
+        tempList = tempState.sortAlpha();
+
+        switch (r) {
+            case ASIAN:
+                return tempList[0];
+            case BLACK:
+                return tempList[1];
+            case LATINX:
+                return tempList[2];
+            case OTHER:
+                return tempList[3];
+            case WHITE:
+                return tempList[4];
+            default:
+                return null;
+        }
+
+    }
+
+
+    /**
+     * Sorts the races alphabetically
+     * 
+     * @return alphabetized array
+     */
+    public Race[] sortAlpha() {
+
+        return null;
+    }
+
+
+    /**
+     * 
+     * Sorts the races by CFR
+     * 
+     * @return sorted array
+     */
+    public Race[] sortCFR() {
+        return null;
+    }
 
 }
