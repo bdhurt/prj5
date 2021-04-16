fe6510730a282dc5e6230872bf6d893a8dd2dd9b
bdhurt <82672726+bdhurt@users.noreply.github.com>
2021-04-16 14:52:04 -0400

    Began StateTest
    Added empty methods and comments in StateTest

--- src/prj5/StateTest.java
+++ src/prj5/StateTest.java
@@ -2,15 +2,48 @@ package prj5;
 
 /**
  * Test class for the State class
+ * 
  * @author benjaminhurt
  *
  */
-public class StateTest extends student.TestCase{
+public class StateTest extends student.TestCase {
 
     /**
      * Sets up the test class
      */
     public void setUp() {
-        
+
+    }
+
+
+    /**
+     * Tests the getRace method
+     */
+    public void testGetRace() {
+
+    }
+
+
+    /**
+     * Tests the sortAlpha method
+     */
+    public void testSortAlpha() {
+
+    }
+
+
+    /**
+     * Tests the sortCFR method
+     */
+    public void testSortCFR() {
+
+    }
+
+
+    /**
+     * Tests the getName method
+     */
+    public void testGetName() {
+
     }
 }
