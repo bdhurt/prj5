69da63e62cc28a56f162ecb8f4ca9df68e4815f0
bdhurt <82672726+bdhurt@users.noreply.github.com>
2021-04-16 14:11:54 -0400

    Added empty main method to Input

--- src/prj5/Input.java
+++ src/prj5/Input.java
@@ -1,15 +1,22 @@
 package prj5;
 
-
 /**
  * Reads input
+ * 
  * @author jared
  * @author benjaminhurt
  * @author Anthony Yermas
- * 
  *
  */
 public class Input {
-    
 
+    /**
+     * Main method for the project runner
+     * 
+     * @param args
+     *            optional input values
+     */
+    public static void main(String[] args) {
+
+    }
 }
