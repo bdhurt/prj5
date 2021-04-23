33042060acfbc7777b91e3dca7425462486eed81
bdhurt <82672726+bdhurt@users.noreply.github.com>
2021-04-23 17:55:20 -0400

    Update FileReaderTest.java

--- src/prj5/FileReaderTest.java
+++ src/prj5/FileReaderTest.java
@@ -1,5 +1,8 @@
 package prj5;
 
+import java.io.FileNotFoundException;
+import java.text.ParseException;
+
 /**
  * Tests the FileReader class
  * 
@@ -7,6 +10,15 @@ package prj5;
  * @version 04.23.2021
  *
  */
-public class FileReaderTest {
-
+public class FileReaderTest extends student.TestCase{
+    private FileReader reader;
+    
+    public void setUp() throws FileNotFoundException, ParseException {
+        reader = new FileReader("Cases_and_Deaths_by_race_CRDT_Sep2020.csv");
+    }
+    
+    
+    public void testFileReader() {
+        
+    }
 }
