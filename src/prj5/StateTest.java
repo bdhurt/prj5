c941a1ae8ad9c251a2adbf312742fd26308f09e9
WWII468 <jaredo19@vt.edu>
2021-04-23 16:09:59 -0400

    Update StateTest.java

--- src/prj5/StateTest.java
+++ src/prj5/StateTest.java
@@ -9,9 +9,39 @@ package prj5;
 public class StateTest extends student.TestCase {
 
     /**
-     * Sets up the test class
+     * test white race
+     */
+    private Race testWhite;
+    
+    /**
+     * test black race
+     */
+    private Race testBlack;
+    
+    /**
+     * test latinx race
+     */
+    private Race testLatinx;
+    
+    /**
+     * test asian race
+     */
+    private Race testAsian;
+    
+    /**
+     * test other race
+     */
+    private Race testOther;
+    
+    /**
+     * Sets up the test methods before they run
      */
     public void setUp() {
+        testWhite = new Race("white",100,8);
+        testBlack = new Race("black",150,10);
+        testLatinx = new Race("Latinx",240,30);
+        testAsian = new Race("Asian",422,6);
+        testOther = new Race("other",80,16);
 
     }
 
