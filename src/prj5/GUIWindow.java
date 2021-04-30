cda6b8e051e5894f83ffd4e035d6fddcb11e42c8
bdhurt <82672726+bdhurt@users.noreply.github.com>
2021-04-30 14:18:31 -0400

    Added empty methods for click methods and drawBars, cleaned up comments

--- src/prj5/GUIWindow.java
+++ src/prj5/GUIWindow.java
@@ -5,8 +5,6 @@ import cs2.Window;
 import cs2.Button;
 import cs2.WindowSide;
 import java.awt.Color;
-import java.util.Iterator;
-import java.util.NoSuchElementException;
 
 /**
  * Creates the window in which we view the COVID visualization
@@ -29,28 +27,12 @@ public class GUIWindow {
     private Window window;
 
     /**
-     * The bar representing the white population
+     * Creates the bars displayed in the window
      */
     private Shape barP1;
-
-    /**
-     * The bar representing the black population
-     */
     private Shape barP2;
-
-    /**
-     * The bar representing the Latinx population
-     */
     private Shape barP3;
-
-    /**
-     * The bar representing the Asian population
-     */
     private Shape barP4;
-
-    /**
-     * The bar representing the other population
-     */
     private Shape barP5;
 
     /**
@@ -59,12 +41,12 @@ public class GUIWindow {
     public static final int WIDTH = 10;
 
     /**
-     * x coordinate of our first bar
+     * X coordinate of our first bar
      */
     public static final int BAR_X = 100;
 
     /**
-     * y coordinate of our first bar
+     * Y coordinate of the bars
      */
     public static final int BAR_Y = 100;
 
@@ -78,48 +60,64 @@ public class GUIWindow {
     public GUIWindow(LinkedList<State> windowList) {
         this.windowList = windowList;
 
-        // creates a new window named COVID Visualization
+        // Creates a new window named COVID Visualization
         window = new Window("COVID Visualization");
 
-        // creates our white population bar
+        // Makes default bars
         barP1 = new Shape(BAR_X, BAR_Y, WIDTH, 0, Color.BLUE);
         barP2 = new Shape(BAR_X, BAR_Y, WIDTH, 0, Color.BLUE);
         barP3 = new Shape(BAR_X, BAR_Y, WIDTH, 0, Color.BLUE);
         barP4 = new Shape(BAR_X, BAR_Y, WIDTH, 0, Color.BLUE);
         barP5 = new Shape(BAR_X, BAR_Y, WIDTH, 0, Color.BLUE);
+        // Draws the bars using the first state
         drawShapes(0);
 
-        
-        // creates our quit button
+        // Creates our quit button
         Button quitButton = new Button("Quit");
-
-        // tells our button what to do when it's clicked
         quitButton.onClick(this, "clickedQuit");
-
-        // adds our Solve button to the top of the window
         window.addButton(quitButton, WindowSide.NORTH);
-        
-        // creates our solve button
+
+        // Creates our sort by alpha button
         Button alphaButton = new Button("Sort by Alpha");
-
-        // tells our button what to do when it's clicked
         alphaButton.onClick(this, "clickedAlpha");
-
-        // adds our Solve button to the top of the window
         window.addButton(alphaButton, WindowSide.NORTH);
-        
-        // creates our solve button
+
+        // Creates our Sort by CFR button
         Button cfrButton = new Button("Sort by CFR");
-
-        // tells our button what to do when it's clicked
         cfrButton.onClick(this, "clickedCFR");
-
-        // adds our Solve button to the top of the window
         window.addButton(cfrButton, WindowSide.NORTH);
     }
-    
+
+
+    /**
+     * Changes the height of the bars
+     * 
+     * @param listIndex
+     *            the state that is being displayed
+     */
     private void drawShapes(int listIndex) {
-        
+
+    }
+
+
+    /**
+     * Sorts alphabetically and redraws the bars when clicked
+     */
+    private void clickedAlpha() {
+
+    }
+
+
+    /**
+     * Sorts by CFR and redraws the bars when clicked
+     */
+    private void clickedCFR() {
+
+    }
+
+
+    private void clickedQuit() {
+        System.exit(0);
     }
 
 }
